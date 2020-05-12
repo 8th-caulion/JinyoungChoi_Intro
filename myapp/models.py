83f1f5ac75863266cb00aea1679226819b05973a
Choijin-young <miraclefocus13@naver.com>
2020-05-12 13:58:07 +0900

    [UPDATE 댓글기능시도]

--- myapp/models.py
+++ myapp/models.py
@@ -13,3 +13,18 @@ class Blog(models.Model):
 
     def summary(self):
         return self.body[:40]
+
+#여기부터 comment
+class Comment(models.Model):
+    post = models.ForeignKey('myapp.Blog', on_delete=models.CASCADE, related_name='comments')
+    author = models.CharField(max_length=200)
+    text = models.TextField()
+    pub_date = models.DateTimeField('date published')
+    approved_comment = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+
+    def __str__(self):
+        return self.text
